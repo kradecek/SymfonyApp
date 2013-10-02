f0ca863728f90d854dbdfae4e59566e96f02e57d
radek <radek@radek-laptop>
2013-10-02 23:31:55 +0200

    KradecekLayoutBundle   - Příprava login formuláře   - Úprava použití Assetic   - Přehled uživatelů

--- src/Kradecek/LayoutBundle/Controller/DefaultController.php
+++ src/Kradecek/LayoutBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Kradecek\LayoutBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
@@ -18,6 +19,25 @@ class DefaultController extends Controller
 
     public function loginAction()
     {
-        return $this->render('KradecekLayoutBundle:Default:login.html.twig');
+        $defaultData = array('message' => 'Type your message here');
+        $form = $this->createFormBuilder($defaultData)
+        ->add('email', 'email', array('label' => 'Email address'))
+        ->add('password', 'password', array('label' => 'Password'))
+        ->add('send', 'submit', array('label' => 'Sign in'))
+        ->getForm();
+        return $this->render('KradecekLayoutBundle:Default:login.html.twig', array('form' => $form->createView()));
+    }
+    
+    /**
+     * @Template
+     */
+    public function usersAction() {
+        $users = array();
+        for($i = 1; $i <= 10; ++$i) {
+            $users[] = array('id' => $i, 'email' => "user$i@example.com");
+        }
+        
+        // If I use @Template anotation (without arguments) then Symfony finds the view for this action
+        return array('users' => $users);
     }
 }
